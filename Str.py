87d1c391c45634651dbf64467cef6ad1d533fbd2
庄文岐 <z1531638853@163.com>
2022-01-24 15:51:01 +0800

    新增python文件

---
+++ Str.py
@@ -0,0 +1,12 @@
+str='Runoob'
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str[1:5:2])
+print(str*2)
+print(str+'  nihao')
+print('============================')
+print('hello\nrunoob')
+print(r'hello\nrunoob')
---
+++ hello.py
@@ -0,0 +1,7 @@
+#第一行注释
+def a = True
+if a==True
+	print("true")
+else
+	print("False")
+print("hello word")#第二个注释
